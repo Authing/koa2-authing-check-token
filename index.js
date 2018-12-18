e9a297b381d62f93e27138b9e47bd4fd54a7a035
Mark Yeh <yelexin@hotmail.com>
2018-12-18 20:20:38 +0800

    feat: check login status with ctx.request.authing

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = () => {
+    return async (ctx, next) => {
+        if (!ctx.request.authing) {
+            throw new Error('ctx.request.authing 不存在，此中间件需结合 koa2-authing 使用');
+        }
+        let token = ctx.request.body.token || ctx.request.query.token || ctx.request.header.token;
+        if(!token) {
+            ctx.status = 401;
+            ctx.body = {errno: 2401, msg: 'token 不能为空', data: null};
+            return
+        }
+        try {
+            let result = await ctx.request.authing.checkLoginStatus(token);
+            if(result.status === true) {
+                await next();
+            } else {
+                ctx.status = 401;
+                ctx.body = {errno: 2402, msg: result.message, code: result.code, data: null};
+            }
+        } catch (e) {
+            console.log(e);
+            ctx.status = 500;
+            ctx.body = {errno: 2403, msg: 'Authing checkLoginStatus 服务失败', data: null};
+        }
+    };
+};
